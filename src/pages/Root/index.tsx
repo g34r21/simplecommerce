3fe6023a3af4da3068d913360b8408189072e123
g34r21 <gear21@icloud.com>
2023-06-19 23:20:18 -0500

    refactor: adjust masonry styles

--- src/pages/Root/index.tsx
+++ src/pages/Root/index.tsx
@@ -1,21 +1,27 @@
 import './Root.css';
 
 import type { FC } from 'react';
-import { Outlet } from 'react-router-dom';
+import { useOutlet, useParams } from 'react-router-dom';
 
 import ProductsMasonry from '~/components/ProductsMasonry';
 
-const RootPage: FC = () => (
-  <div className="root-container">
-    <div className="left-side">
-      <h2 className="store__heading">Store</h2>
-      <hr className="hr-secondary store__rule" />
-      <ProductsMasonry />
+const RootPage: FC = () => {
+  const { productId } = useParams();
+  const outlet = useOutlet();
+  return (
+    <div className="root-container">
+      <div className="left-side">
+        <h2 className="store__heading">Store</h2>
+        <hr className="hr--secondary store__rule" />
+        <ProductsMasonry />
+      </div>
+      <div className="right-side">
+        <h2 className="store__heading">{productId ? 'Product' : ''}</h2>
+        <hr className="hr--secondary store__rule" />
+        {outlet || <p className="placeholder">Please choose a product</p>}
+      </div>
     </div>
-    <div className="right-side">
-      <Outlet />
-    </div>
-  </div>
-);
+  );
+};
 
 export default RootPage;
