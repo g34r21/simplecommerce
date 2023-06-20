4e690fc6fe097162757b9940ee2f4e1bd11cb246
g34r21 <gear21@icloud.com>
2023-06-19 23:59:14 -0500

    feat: add links to products in catalog

--- src/components/ProductsMasonry/index.tsx
+++ src/components/ProductsMasonry/index.tsx
@@ -2,6 +2,7 @@ import './ProductsMasonry.css';
 
 import { type FC } from 'react';
 import { useSelector } from 'react-redux';
+import { Link, useParams } from 'react-router-dom';
 
 import useLoadProducts from '~/hooks/useLoadProducts';
 import { getProducts } from '~/store/selectors/catalog';
@@ -9,21 +10,26 @@ import { getProducts } from '~/store/selectors/catalog';
 const ProductsMasonry: FC = () => {
   const products = useSelector(getProducts);
   useLoadProducts();
+  const { productId } = useParams();
 
   return (
     <div className="products-masonry__container">
       {Object.entries(products).map(([key, product]) => (
         <div
-          data-testid="productContainer"
-          className="products-masonry__card"
           key={key}
+          className={`products-masonry__card ${
+            productId === key ? 'products-masonry__card--active' : ''
+          }`}
+          data-testid="productContainer"
         >
-          <img
-            data-testid="productImage"
-            className="products-masonry__image"
-            src={product.image}
-            alt={product.title}
-          />
+          <Link to={`/products/${key}`}>
+            <img
+              data-testid="productImage"
+              className="products-masonry__image"
+              src={product.image}
+              alt={product.title}
+            />
+          </Link>
         </div>
       ))}
     </div>
